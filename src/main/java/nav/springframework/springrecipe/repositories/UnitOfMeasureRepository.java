2bd75dfb88ac296c7ec5d3d81fe8933a6e58525a
nav <nico.voormanns@orchit.de>
2022-06-02 15:28:40 +0200

    Spring Data JPA Query Methods

--- src/main/java/nav/springframework/springrecipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/nav/springframework/springrecipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package nav.springframework.springrecipe.repositories;
 import nav.springframework.springrecipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
