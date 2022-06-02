bc16d914bc2827f39db34956afa7aaa84c8fcaad
nav <nico.voormanns@orchit.de>
2022-06-02 09:42:17 +0200

    Initial commit

---
+++ src/main/java/nav/springframework/springrecipe/SpringrecipeApplication.java
@@ -0,0 +1,13 @@
+package nav.springframework.springrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nav/springframework/springrecipe/SpringrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package nav.springframework.springrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
