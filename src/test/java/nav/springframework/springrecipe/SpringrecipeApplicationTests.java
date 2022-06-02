fe45a9a7dfe506d98ad688fc7d091fd79433f3be
nav <nico.voormanns@orchit.de>
2022-06-02 14:00:39 +0200

    One to One JPA Relationships

--- src/test/java/nav/springframework/springrecipe/SpringrecipeApplicationTests.java
+++ src/test/java/nav/springframework/springrecipe/SpringrecipeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringrecipeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
