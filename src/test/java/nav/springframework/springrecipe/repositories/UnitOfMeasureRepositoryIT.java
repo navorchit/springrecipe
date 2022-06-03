e115baec620e15a5dc6101d6f33cf7f1c1597f92
nav <nico.voormanns@orchit.de>
2022-06-03 14:37:20 +0200

    Spring Integration Test

---
+++ src/test/java/nav/springframework/springrecipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,31 @@
+package nav.springframework.springrecipe.repositories;
+
+import nav.springframework.springrecipe.model.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+}
