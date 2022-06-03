f426f56be9b5bf431f0d1e6c6c2f4d8b83365ccc
nav <nico.voormanns@orchit.de>
2022-06-03 14:04:32 +0200

    Assignment: Write Test for IndexController

---
+++ src/test/java/nav/springframework/springrecipe/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package nav.springframework.springrecipe.controllers;
+
+import nav.springframework.springrecipe.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+    @Mock
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String indexPage = indexController.getIndexPage(model);
+
+        assertEquals("index", indexPage);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+
+    }
+}
