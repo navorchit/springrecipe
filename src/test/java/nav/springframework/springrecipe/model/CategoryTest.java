18e6a52f444d456aa418490a79b1faa7faf8f09f
nav <nico.voormanns@orchit.de>
2022-06-03 13:38:32 +0200

    Creating a JUnit Test

---
+++ src/test/java/nav/springframework/springrecipe/model/CategoryTest.java
@@ -0,0 +1,32 @@
+package nav.springframework.springrecipe.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setup() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
